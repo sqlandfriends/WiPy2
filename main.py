50af40c50b456d2d28e76102dbf26bb3d5f2038a
Horacy Piguła <sqlandfriends@yahoo.com>
2017-07-10 17:00:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,10 @@ from network import WLAN
 wlan = WLAN(mode=WLAN.STA)
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'Orange-F813':
+   
+    elif net.ssid == 'Net Name': #where Net Name is Your network name
         print('Network found!')
-        wlan.connect('Orange-F813', auth=(WLAN.WPA2, '73044277'), timeout=5000)
-       
-    elif net.ssid == 'dlink':
-        print('Network found!')
-        wlan.connect('dlink', timeout=5000)
-    elif net.ssid == 'VMD5BC68E':
-        print('Network found!')
-        wlan.connect('VMD5BC68E',auth=(WLAN.WPA2, 'Ge6nGnvszvhd'), timeout=5000)
+        wlan.connect('Net Name',auth=(WLAN.WPA2, 'Pass'), timeout=5000)#Pass = Password for Your network
     
         while not wlan.isconnected():
             machine.idle() # save power while waiting
